3d76e55bac35b06331b169a4b5ae3219d34dce90
Ming <13314973+MingMrs@user.noreply.gitee.com>
2024-03-14 17:36:20 +0800

    first commit

--- consul/consul.go
+++ consul/consul.go
@@ -7,40 +7,40 @@ import (
 )
 
 // consul服务发现
-func AgentHealthService(serviceName string) (string, error) {
-	fmt.Println("我进到consul发现里面了")
-	client, err := capi.NewClient(capi.DefaultConfig()) //创建consul客户端
-	if err != nil {
-		return "", err
-	}
-	sr, info, err := client.Agent().AgentHealthServiceByName(serviceName) //健康查询
-	if err != nil {
-		return "", err
-	}
-	if sr != "passing" { //如果健康状态不是 "passing"，则返回一个错误。
-		return "", fmt.Errorf("is not have health service")
-	}
-	for _, v := range info {
-		fmt.Println("consul:", v)
-	}
-	//如果健康状态为 "passing"，则将其中一个健康实例的地址返回。
-	return fmt.Sprintf("%v:%v", info[0].Service.Address, info[0].Service.Port), nil
-}
+//func AgentHealthService(serviceName string) (*grpc.ClientConn,error){
+//	return grpc.Dial("consul://10.2.171.70:8500/"+serviceName+"?wait=14s", grpc.WithInsecure(), grpc.WithDefaultServiceConfig(`{"LoadBalancingPolicy": "round_robin"}`))
+//
+//	//fmt.Println("我进到consul发现里面了")
+//	//client, err := capi.NewClient(capi.DefaultConfig()) //创建consul客户端
+//	//if err != nil {
+//	//	return "", err
+//	//}
+//	//sr, info, err := client.Agent().AgentHealthServiceByName(serviceName) //健康查询
+//	//if err != nil {
+//	//	return "", err
+//	//}
+//	//if sr != "passing" { //如果健康状态不是 "passing"，则返回一个错误。
+//	//	return "", fmt.Errorf("is not have health service")
+//	//}
+//	//for _, v := range info {
+//	//	fmt.Println("consul:", v)
+//	//}
+//	////如果健康状态为 "passing"，则将其中一个健康实例的地址返回。
+//	//return fmt.Sprintf("%v:%v", info[0].Service.Address, info[0].Service.Port), nil
+//}
 
 // consul服务注册
 func ServiceRegister(address, port, ip, ConsulPort string) error {
+	clien := capi.DefaultConfig()
+	clien.Address = fmt.Sprintf("%v,%v", address, ConsulPort)
 	client, err := capi.NewClient(capi.DefaultConfig())
 	if err != nil {
 		return err
 	}
-
 	portInt, err := strconv.Atoi(port)
 	if err != nil {
 		return err
 	}
-
-	capi.DefaultConfig().Address = fmt.Sprintf("%v,%v", address, ConsulPort)
-
 	return client.Agent().ServiceRegister(&capi.AgentServiceRegistration{
 		ID:      "test",
 		Name:    "user",
--- grpc/client.go
+++ grpc/client.go
@@ -1,17 +1,14 @@
 package grpc
 
 import (
-	"fmt"
-	"github.com/Minggyyds/framework/consul"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func Client(toService string) (*grpc.ClientConn, error) {
-	conn, err := consul.AgentHealthService(toService)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println(conn)
-	return grpc.Dial(conn, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//conn, err := consul.AgentHealthService(toService)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//fmt.Println(conn)
+	return grpc.Dial("consul://10.2.171.85:8500/"+toService+"?wait=14s", grpc.WithInsecure(), grpc.WithDefaultServiceConfig(`{"LoadBalancingPolicy": "round_robin"}`))
 }
