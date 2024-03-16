537fb929eec871423fe5b4fa6009c9a22536cc0c
Ming <13314973+MingMrs@user.noreply.gitee.com>
2024-03-16 09:01:55 +0800

    first commit

--- grpc/client.go
+++ grpc/client.go
@@ -2,22 +2,24 @@ package grpc
 
 import (
 	"fmt"
+	"github.com/Minggyyds/framework/consul"
 	_ "github.com/mbobakov/grpc-consul-resolver"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func Client(toService string) (*grpc.ClientConn, error) {
-
-	cof, err := GetConfig(toService)
-	if err != nil {
-		return nil, err
-	}
-	//conn, err := consul.AgentHealthService(toService)
+	//cof, err := GetConfig(toService)
 	//if err != nil {
 	//	return nil, err
 	//}
-	//fmt.Println(conn)
+	conn, err := consul.AgentHealthService(toService)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(conn)
+	return grpc.Dial(conn, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	//return grpc.Dial("10.2.171.85:8500", grpc.WithInsecure(), grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "round_robin", "service": {"method": "%s"}}`, toService)))
-	consulURL := fmt.Sprintf("consul://%s:%s/+%s+?wait=14s", cof.App.Ip, cof.App.ConsulPort, toService)
-	return grpc.Dial(consulURL, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(`{"LoadBalancingPolicy": "round_robin"}`))
+	//consulURL := fmt.Sprintf("consul://%s:%s/+%s+?wait=14s", cof.App.Ip, cof.App.ConsulPort, toService)
+	//return grpc.Dial(consulURL, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(`{"LoadBalancingPolicy": "round_robin"}`))
 }
